68669bfbd2f19c216f1e0c8afbe3f0dc0f35df10
GA1ACTICA <2008.olle@gmail.com>
2026-07-10 18:41:44 +0200

    build: :construction_worker: add build for javadoc webbsite

--- engine/src/main/java/gameEngine/engineModules/cursor/CursorType.java
+++ engine/src/main/java/gameEngine/engineModules/cursor/CursorType.java
@@ -88,193 +88,193 @@ public final class CursorType {
     }
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/alias.png" alt="Alias
+     * <img src="../../../cursors/alias.png" alt="Alias
      * cursor">
      */
     public static CursorType ALIAS = new CursorType("alias.png", new Point(72, 14), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/all-resize.png" alt="All
+     * <img src="../../../cursors/all-resize.png" alt="All
      * resize cursor">
      */
     public static CursorType ALL_RESIZE = new CursorType("all-resize.png", new Point(48, 44), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/all-scroll.png" alt="All
+     * <img src="../../../cursors/all-scroll.png" alt="All
      * scroll cursor">
      */
     public static CursorType ALL_SCROLL = new CursorType("all-scroll.png", new Point(44, 44), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/cell.png" alt="Cell cursor">
+     * <img src="../../../cursors/cell.png" alt="Cell cursor">
      */
     public static CursorType CELL = new CursorType("cell.png", new Point(44, 44), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/col-resize.png" alt="Col
+     * <img src="../../../cursors/col-resize.png" alt="Col
      * resize cursor">
      */
     public static CursorType COL_RESIZE = new CursorType("col-resize.png", new Point(48, 48), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/context-menu.png" alt=
+     * <img src="../../../cursors/context-menu.png" alt=
      * "Context menu cursor">
      */
     public static CursorType CONTEXT_MENU = new CursorType("context-menu.png", new Point(12, 4), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/copy.png" alt="Copy cursor">
+     * <img src="../../../cursors/copy.png" alt="Copy cursor">
      */
     public static CursorType COPY = new CursorType("copy.png", new Point(12, 4), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/crosshair.png" alt="Crosshair
+     * <img src="../../../cursors/crosshair.png" alt="Crosshair
      * cursor">
      */
     public static CursorType CROSSHAIR = new CursorType("crosshair.png", new Point(44, 44), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/default.png" alt="Default
+     * <img src="../../../cursors/default.png" alt="Default
      * cursor">
      */
     public static CursorType DEFAULT = new CursorType("default.png", new Point(12, 4), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/e-resize.png" alt="East
+     * <img src="../../../cursors/e-resize.png" alt="East
      * resize cursor">
      */
     public static CursorType E_RESIZE = new CursorType("e-resize.png", new Point(76, 52), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/ew-resize.png" alt="East,
+     * <img src="../../../cursors/ew-resize.png" alt="East,
      * west
      * resize cursor">
      */
     public static CursorType EW_RESIZE = new CursorType("ew-resize.png", new Point(48, 48), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/grab.png" alt="Grab cursor">
+     * <img src="../../../cursors/grab.png" alt="Grab cursor">
      */
     public static CursorType GRAB = new CursorType("grab.png", new Point(44, 8), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/grabbing.png" alt="Grabbing
+     * <img src="../../../cursors/grabbing.png" alt="Grabbing
      * cursor">
      */
     public static CursorType GRABBING = new CursorType("grabbing.png", new Point(36, 20), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/help.png" alt="Help cursor">
+     * <img src="../../../cursors/help.png" alt="Help cursor">
      */
     public static CursorType HELP = new CursorType("help.png", new Point(48, 84), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/move.png" alt="Move cursor">
+     * <img src="../../../cursors/move.png" alt="Move cursor">
      */
     public static CursorType MOVE = new CursorType("move.png", new Point(12, 4), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/ne-resize.png" alt=
+     * <img src="../../../cursors/ne-resize.png" alt=
      * "North-east resize cursor">
      */
     public static CursorType NE_RESIZE = new CursorType("ne-resize.png", new Point(60, 40), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/nesw-resize.png" alt=
+     * <img src="../../../cursors/nesw-resize.png" alt=
      * "North-east, south-west resize cursor">
      */
     public static CursorType NESW_RESIZE = new CursorType("nesw-resize.png", new Point(44, 44), true);
     /**
      * 
-     * <img src="../../../resources/cursors/Adwaita/no-drop.png" alt="No drop
+     * <img src="../../../cursors/no-drop.png" alt="No drop
      * cursor">
      */
     public static CursorType NO_DROP = new CursorType("no-drop.png", new Point(12, 4), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/not-allowed.png" alt="Not
+     * <img src="../../../cursors/not-allowed.png" alt="Not
      * allowed cursor">
      */
     public static CursorType NOT_ALLOWED = new CursorType("not-allowed.png", new Point(48, 48), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/n-resize.png" alt="North
+     * <img src="../../../cursors/n-resize.png" alt="North
      * resize cursor">
      */
     public static CursorType N_RESIZE = new CursorType("n-resize.png", new Point(52, 24), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/ns-resize.png" alt="North,
+     * <img src="../../../cursors/ns-resize.png" alt="North,
      * south resize cursor">
      */
     public static CursorType NS_RESIZE = new CursorType("ns-resize.png", new Point(48, 52), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/nwse-resize.png" alt=
+     * <img src="../../../cursors/nwse-resize.png" alt=
      * "North-west, south-east resize cursor">
      */
     public static CursorType NWSE_RESIZE = new CursorType("nwse-resize.png", new Point(44, 44), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/pointer.png" alt="Pointer
+     * <img src="../../../cursors/pointer.png" alt="Pointer
      * cursor">
      */
     public static CursorType POINTER = new CursorType("pointer.png", new Point(28, 20), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/row-resize.png" alt="Row
+     * <img src="../../../cursors/row-resize.png" alt="Row
      * resize cursor">
      */
     public static CursorType ROW_RESIZE = new CursorType("row-resize.png", new Point(48, 52), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/se-resize.png" alt=
+     * <img src="../../../cursors/se-resize.png" alt=
      * "South-east resize cursor">
      */
     public static CursorType SE_RESIZE = new CursorType("se-resize.png", new Point(60, 60), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/s-resize.png" alt="South
+     * <img src="../../../cursors/s-resize.png" alt="South
      * resize cursor">
      */
     public static CursorType S_RESIZE = new CursorType("s-resize.png", new Point(52, 72), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/sw-resize.png" alt=
+     * <img src="../../../cursors/sw-resize.png" alt=
      * "South-west resize cursor">
      */
     public static CursorType SW_RESIZE = new CursorType("sw-resize.png", new Point(40, 60), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/text.png" alt="Text cursor">
+     * <img src="../../../cursors/text.png" alt="Text cursor">
      */
     public static CursorType TEXT = new CursorType("text.png", new Point(44, 48), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/vertical-text.png" alt=
+     * <img src="../../../cursors/vertical-text.png" alt=
      * "Vertical text cursor">
      */
     public static CursorType VERTICAL_TEXT = new CursorType("vertical-text.png", new Point(48, 44), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/w-resize.png" alt="West
+     * <img src="../../../cursors/w-resize.png" alt="West
      * resize cursor">
      */
     public static CursorType W_RESIZE = new CursorType("w-resize.png", new Point(24, 52), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/x-cursor.png" alt="X cursor">
+     * <img src="../../../cursors/x-cursor.png" alt="X cursor">
      */
     public static CursorType X_CURSOR = new CursorType("x-cursor.png", new Point(44, 48), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/zoom-in.png" alt="Zoom in
+     * <img src="../../../cursors/zoom-in.png" alt="Zoom in
      * cursor">
      */
     public static CursorType ZOOM_IN = new CursorType("zoom-in.png", new Point(44, 40), true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/zoom-out.png" alt="Zoom out
+     * <img src="../../../cursors/zoom-out.png" alt="Zoom out
      * cursor">
      */
     public static CursorType ZOOM_OUT = new CursorType("zoom-out.png", new Point(44, 40), true);
@@ -282,13 +282,13 @@ public final class CursorType {
     // --- Animated ---
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/progress/progress_0001.png"
+     * <img src="../../../cursors/progress/progress_0001.png"
      * alt="Progress cursor">
      */
     public static CursorType PROGRESS = new CursorType("progress", null, true);
 
     /**
-     * <img src="../../../resources/cursors/Adwaita/wait/wait_0001.png" alt="Wait
+     * <img src="../../../cursors/wait/wait_0001.png" alt="Wait
      * cursor">
      */
     public static CursorType WAIT = new CursorType("wait", null, true);
